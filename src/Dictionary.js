be7e68a4879f6ce9c3455930c3b4f58075397c36
SarahKatherineFrancis <sarah.katherinefrancis@gmail.com>
2022-03-13 17:04:10 +0800

    API integration

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,10 +1,16 @@
 import React, { useState } from "react";
+import axios from "axios";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
+
+  function handleResponse(response) {
+    console.log(response.data[0]);
+  }
   function search(event) {
     event.preventDefault();
-    console.log(keyword);
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
+    axios.get(apiUrl).than(handleResponse);
   }
   function handleSubmit(event) {
     setKeyword(event.target.value);
