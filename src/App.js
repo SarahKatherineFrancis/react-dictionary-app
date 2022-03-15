a67b8c37f344f718fafa7bc1a3662b022b74786c
SarahKatherineFrancis <sarah.katherinefrancis@gmail.com>
2022-03-15 11:13:07 +0800

    Changed colour layout and added logo

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import logo from "./logo.png";
 import "./App.css";
 import Dictionary from "./Dictionary";
 
@@ -5,12 +6,20 @@ function App() {
   return (
     <div className="App">
       <div className="container">
-        <Dictionary defaultKeyword="purple" />
+        <header className="App-header">
+          <img src={logo} className="App-logo img-fluid" alt="logo" />
+        </header>
+        <Dictionary defaultKeyword="candy" />
       </div>
       <footer>
         Coded by {""}
-        <a href="https://github.com/SarahKatherineFrancis/react-dictionary-app">
+        <a href="https://friendly-bose-968c32.netlify.app/">
           Sarah Francis
+        </a>{" "}
+        and is{" "}
+        <a href="https://github.com/SarahKatherineFrancis?tab=repositories">
+          {" "}
+          open-sourced on GitHub
         </a>
       </footer>
     </div>
